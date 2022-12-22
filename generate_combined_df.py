54f215646ccf0048e29ab215587594e233cb54af
Zendelo <s3806763@student.rmit.edu.au>
2022-12-22 17:10:04 +1100

    separate code into files

---
+++ generate_combined_df.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+
+def read_queries_df(ndcg_df, ranks_sr, rates_sr):
+    qdf = pd.read_csv('data/QueriesSurvey.csv', header=None, names=['qid', 'query'], index_col='qid').applymap(
+        str.strip)
+    qdf.index = qdf.index.str.strip()
+    qdf = qdf.merge(ndcg_df, left_index=True, right_index=True)
+    qdf['topic'] = qdf.index.str.split('-').str[0]
+    qdf = qdf.assign(avg_rate=qdf['query'].map(rates_sr), avg_rank=qdf['query'].map(ranks_sr))
+    return qdf
+
+
+def read_survey_data(ranks_df_path, ratings_df_path):
+    col_names = ['rid', 'topic', 'query', 'value', 'wid', 'batch', 'duration', 'EndDate', 'query_mean']
+    dtypes = {'rid': str, 'topic': str, 'query': str, 'value': int, 'wid': str, 'batch': str, 'duration': int,
+              'query_mean': float}
+    ranks_df = pd.read_csv(ranks_df_path, index_col='rid', names=col_names, dtype=dtypes, header=0,
+                           parse_dates=['EndDate'])
+    rates_df = pd.read_csv(ratings_df_path, index_col='rid', names=col_names, dtype=dtypes, header=0,
+                           parse_dates=['EndDate'])
+    ranks_df['value'] = ranks_df['value'].max() - ranks_df['value'] + 1
+
+    return ranks_df, rates_df
+
+
+def read_unique_users_queries(unique_user_queries_path):
+    unique_user_queries = pd.read_csv(unique_user_queries_path, header=None, names=['qid', 'query'], index_col='query')
+    return unique_user_queries.assign(rid=unique_user_queries.qid.str.rsplit('-', 1).str[1])
+
+
+def read_ndcg_df(ndcg_df_path):
+    return pd.read_csv(ndcg_df_path, sep='\t', header=None, names=['qid', f'nDCG@10'], index_col='qid')
+
+
+def read_all_users_queries(all_user_queries_path, unique_user_queries_df, users_ndcg_df):
+    all_user_queries = pd.read_csv(all_user_queries_path, header=None,
+                                   names=['qid', 'user_query']).assign(
+        topic=lambda x: x.qid.apply(lambda y: y.split('-')[0])).sort_values('qid')
+    all_user_queries = all_user_queries.assign(rid=all_user_queries.qid.str.rsplit('-', 1).str[1])
+    all_user_queries = all_user_queries.assign(method=all_user_queries.qid.str.split('-').str[1].str.capitalize())
+    all_user_queries = all_user_queries.assign(topic=all_user_queries.qid.str.split('-').str[0])
+
+    all_user_queries['ref_qid'] = all_user_queries['user_query'].apply(lambda x: unique_user_queries_df.loc[x, 'qid'])
+    all_user_queries[f'user_nDCG@10'] = all_user_queries['ref_qid'].apply(lambda x: users_ndcg_df.loc[x, f'nDCG@10'])
+    # all_user_queries.set_index(['method', 'topic', 'rid'])
+    return all_user_queries
+
+
+def combine_dataframes(ranks_df, rates_df, qdf, all_user_queries):
+    comb_df = pd.concat([ranks_df.assign(method='Ranking'), rates_df.assign(method='Rating')]).sort_values(
+        ['topic', 'method', 'query_mean', 'value'])
+    comb_df['value'].clip(lower=1, inplace=True)  # clipping al rating to start from 1
+
+    # assign batch order per wid by the time finished
+    comb_df = comb_df.assign(batch_order=comb_df.groupby(['wid'])['EndDate'].rank(method="dense"))
+
+    comb_df = comb_df.assign(qid=comb_df['query'].map(qdf.reset_index().set_index('query')['qid']))
+
+    _comb_df = comb_df.reset_index().set_index(['method', 'topic', 'rid']).merge(
+        all_user_queries.set_index(['method', 'topic', 'rid'])[[f'user_nDCG@10', 'user_query']], left_index=True,
+        right_index=True)
+    return _comb_df.reset_index()
+
+
+def main():
+    ranks_df_file = 'data/ranks_df_long.csv'
+    ratings_df_file = 'data/ratings_df_long.csv'
+    ranks_df, rates_df = read_survey_data(ranks_df_file, ratings_df_file)
+    ndcg_df = pd.read_csv(f'data/PL2.DFR.SD-survey.ndcg@10', sep='\t', header=None, names=['qid', f'nDCG@10'],
+                          index_col='qid')
+    ranks_sr = ranks_df.groupby(['query'])['value'].mean()
+    rates_sr = rates_df.groupby(['query'])['value'].mean()
+    qdf = read_queries_df(ndcg_df, ranks_sr, rates_sr)
+    all_user_queries_path = 'data/user_queries/all_normalized_user_queries.csv'
+    unique_user_queries_path = 'data/user_queries/unique_normalized_user_queries.csv'
+    ndcg_df_path = 'data/user_queries/PL2.DFR.SD.ndcg@10'
+    unique_user_queries_df = read_unique_users_queries(unique_user_queries_path)
+    users_ndcg_df = read_ndcg_df(ndcg_df_path)
+    all_user_queries = read_all_users_queries(all_user_queries_path, unique_user_queries_df, users_ndcg_df)
+    comb_df = combine_dataframes(ranks_df, rates_df, qdf, all_user_queries)
+    print(comb_df.head())
+    print(comb_df.info())
+    comb_df.to_parquet('comb_df.parquet.zstd', compression='zstd')
+    # filter only the accepted queries
+    # all_user_queries = all_user_queries.loc[all_user_queries['rid'].isin(comb_df.index)]
+
+
+if __name__ == '__main__':
+    main()
---
+++ utility_functions.py
@@ -0,0 +1,69 @@
+import re
+
+import numpy as np
+import pandas as pd
+
+TREC_RES_COLUMNS = ['qid', 'iteration', 'docNo', 'rank', 'docScore', 'method']
+TREC_QREL_COLUMNS = ['qid', 'iteration', 'docNo', 'rel']
+
+
+def filter_alpha_numeric(s):
+    return ' '.join(re.sub(r'[^A-Za-z0-9 ]+', '', s).lower().split())
+
+
+def calc_ndcg(qrels_file, results_file, k, base=2, gdeval=True):
+    """
+    Setting gdeval will produce identical results to the official evaluation script that was published for TREC.
+    Note that the calculation in that script differs from (probably) any published research version.
+    :param qrels_file: a path to a TREC style qrels file
+    :param results_file: a path to a TREC style run file (result)
+    :param k: integer to be used as cutoff
+    :param base: a number to be used as the log base for calculation if gdeval=False
+    :param gdeval: boolean parameter that indicates whether to use the gdeval calculation or not
+    :return:
+    """
+
+    # Reading and sorting the qrels, to later speed-up indexing and locating
+    qrels_df = pd.read_csv(qrels_file, delim_whitespace=True, names=TREC_QREL_COLUMNS). \
+        sort_values(['qid', 'rel', 'docNo'], ascending=[True, False, True]).set_index(['qid', 'docNo'])
+    qrels_df['rel'].clip(lower=0, inplace=True)
+
+    # Store beginning and end indices for each query - used for speed up
+    qid_res_len = qrels_df.groupby('qid').apply(len)
+    qid_end_loc = qid_res_len.cumsum()
+    qid_start_loc = qid_end_loc - qid_res_len
+    qrels_df = qrels_df.droplevel(0)
+
+    results_df = pd.read_csv(results_file, delim_whitespace=True, names=TREC_RES_COLUMNS)
+
+    # if calculating according to the gdeval script, the results are sorted by docScore and ties are broken by docNo
+    if gdeval:
+        results_df = results_df.sort_values(['qid', 'docScore', 'docNo'], ascending=[True, False, False]).groupby(
+            'qid').head(k)
+        discount = np.log(np.arange(1, k + 1) + 1)
+    # Otherwise, sort by doc ranks
+    else:
+        results_df = results_df.sort_values(['qid', 'rank']).groupby('qid').head(k)
+        discount = np.concatenate((np.ones(base), np.log(np.arange(base, k) + 1) / np.log(base)))
+
+    result = {}
+    for qid, _df in results_df.groupby('qid'):
+        docs = _df['docNo'].to_numpy()
+        try:
+            _qrels_df = qrels_df.iloc[qid_start_loc.loc[qid]: qid_end_loc.loc[qid]]
+        except KeyError as err:
+            print(f'query id {err} doesn\'t exist in the qrels file, skipping it')
+            continue
+
+        if gdeval:
+            dcg = 2 ** _qrels_df.reindex(docs)['rel'].fillna(0).to_numpy() - 1
+            idcg = ((2 ** _qrels_df['rel'].head(k) - 1) / discount[:len(_qrels_df)]).sum()
+        else:
+            dcg = _qrels_df.reindex(docs)['rel'].fillna(0).to_numpy()
+            idcg = (_qrels_df['rel'].head(k) / discount[:len(_qrels_df)]).sum()
+        result[qid] = (dcg / discount[:len(dcg)]).sum() / idcg
+    res_df = pd.DataFrame.from_dict(result, orient='index', columns=[f'nDCG@{k}'])
+    #     res_df.to_csv(rreplace(results_file, 'run', f'ndcg@{k}', 1), sep='\t', float_format='%.6f', header=False)
+    print(res_df.to_string(float_format='%.5f'))
+    print(f'Mean: {res_df.mean()[0]:.5f}')
+    return res_df
